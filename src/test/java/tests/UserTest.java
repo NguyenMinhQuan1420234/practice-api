e9de33710f18e9d44f945346231e079c9c1fd42d
Minh Quan <banchi0072000@gmail.com>
2022-07-22 11:55:31 +0700

    update

--- src/test/java/tests/UserTest.java
+++ src/test/java/tests/UserTest.java
@@ -1,84 +1,88 @@
 package tests;
 
+import io.restassured.response.Response;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import utils.APIConstant;
 import utils.api.UserHelper;
 
+import static java.lang.Integer.parseInt;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class UserTest {
     UserHelper userHelper = new UserHelper();
 
-//    @Test
-//    public void createUserSuccessfully() {
-//        Response response = userHelper.createUser(NAME, GENDER, EMAIL, STATUS);
-//        response.then()
-//                .assertThat()
-//                .statusCode(200)
-//                .body("code", equalTo(201))
-//                .body("data.name", equalTo(NAME))
-//                .body("data.gender", equalTo(GENDER.toLowerCase()))
-//                .body("data.email", equalTo(EMAIL))
-//                .body("data.status", equalTo(STATUS.toLowerCase()));
-//    }
-//
-//    @Test
-//    public void getUsersSuccessfully() {
-//        Response response = userHelper.getUser(null);
-//        response.then()
-//                .assertThat()
-//                .statusCode(200)
-//                .body("code", equalTo(200))
-//                .body("meta.pagination.total", notNullValue())
-//                .body("meta.pagination.page", notNullValue())
-//                .body("meta.pagination.limit", notNullValue())
-//                .body("data[0]", hasKey("id"))
-//                .body("data[0]", hasKey("name"))
-//                .body("data[0]", hasKey("email"))
-//                .body("data[0]", hasKey("gender"))
-//                .body("data[0]", hasKey("status"));
-//    }
-//
-//    @Test
-//    public void getUserDetailSuccessfully() {
-//        Response response = userHelper.getUser(Integer.toString(USER_ID));
-//        response.then()
-//                .assertThat()
-//                .statusCode(200)
-//                .body("code", equalTo(200))
-//                .body("data.id", equalTo(USER_ID));
-//    }
-//
-//    @Test
-//    public void updateUsersSuccessfully() {
-//        Response response = userHelper.updateUser(USER_ID, UPDATE_NAME, UPDATE_GENDER, UPDATE_EMAIL, UPDATE_STATUS);
-//        response.then()
-//                .assertThat()
-//                .statusCode(200)
-//                .body("code", equalTo(200))
-//                .body("data.id", equalTo(USER_ID))
-//                .body("data.name", equalTo(UPDATE_NAME))
-//                .body("data.gender", equalTo(UPDATE_GENDER.toLowerCase()))
-//                .body("data.email", equalTo(UPDATE_EMAIL))
-//                .body("data.status", equalTo(UPDATE_STATUS.toLowerCase()));
-//        assertThat("Verify status code: ", response.statusCode(), equalTo(200));
-//    }
-//
-//    @Test
-//    public void deleteUsersSuccessfully() {
-//        Response response = userHelper.deleteUser(USER_ID);
-//        response.then()
-//                .assertThat()
-//                .statusCode(200)
-//                .body("code", equalTo(204))
-//                .body("data", equalTo(null));
-//    }
-//
-//    @Test
-//    public void verifyUpdateUserJsonSchema() {
-//        Response response = userHelper.updateUser(UPDATE_USER_ID, null, UPDATE_GENDER, UPDATE_EMAIL, UPDATE_STATUS);
-//        assertThat("Verify status code: ", response.statusCode(), equalTo(200));
-//        userHelper.verifySchema(response, "schema.json");
-//    }
+    @Test
+    public void createUserSuccessfully() {
+        Response response = userHelper.createUser("quan", "emailmotlandung@yahoo.com", "Male", "Active");
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .body("code", equalTo(201))
+                .body("data.name", equalTo("quan"))
+                .body("data.gender", equalTo("Male".toLowerCase()))
+                .body("data.mail", equalTo("emailmotlandung@yahoo.com"))
+                .body("data.status", equalTo("Active".toLowerCase()));
+    }
+
+    @Test
+    public void getUsersInSpecificPageSuccessfully() {
+        Response response = userHelper.getAllUser("2");
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .body("code", equalTo(200))
+                .body("meta.pagination.total", notNullValue())
+                .body("meta.pagination.page", notNullValue())
+                .body("meta.pagination.limit", notNullValue())
+                .body("data[0]", hasKey("id"))
+                .body("data[0]", hasKey("name"))
+                .body("data[0]", hasKey("email"))
+                .body("data[0]", hasKey("gender"))
+                .body("data[0]", hasKey("status"));
+    }
+
+    @Test
+    public void getUserDetailSuccessfully() {
+        Response response = userHelper.getUser(APIConstant.USER_ID);
+        String ID = "";
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .body("code", equalTo(200))
+                .body("data.id", equalTo(APIConstant.USER_ID));
+    }
+
+    @Test
+    public void updateUsersSuccessfully() {
+        Response response = userHelper.updateUserDetails(APIConstant.USER_ID,"Cao Ba Quat","tambaytamba@gmail.com","Female","Inactive");
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .body("code", equalTo(200))
+                .body("data.id", equalTo(parseInt(APIConstant.USER_ID)))
+                .body("data.name", equalTo("Cao Ba Quat"))
+                .body("data.gender", equalTo("Female".toLowerCase()))
+                .body("data.email", equalTo("tambaytamba@gmail.com"))
+                .body("data.status", equalTo("Inactive".toLowerCase()));
+        assertThat("Verify status code: ", response.statusCode(), equalTo(200));
+    }
+
+    @Test
+    public void verifyUpdateUserBySchema() {
+        Response respone = userHelper.updateUserDetails(APIConstant.USER_ID,"Cao Ba Quat","tambaytamba@gmail.com","Female","Inactive");
+        userHelper.verifySchema(respone, "scheme.json");
+    }
+    @Test
+    public void deleteUsersSuccessfully() {
+        Response response = userHelper.deleteUser(APIConstant.USER_ID);
+        response.then()
+                .assertThat()
+                .statusCode(200)
+                .body("code", equalTo(204))
+                .body("data", equalTo(null));
+    }
+
 }
--- src/test/java/utils/api/UserHelper.java
+++ src/test/java/utils/api/UserHelper.java
@@ -2,9 +2,8 @@ package utils.api;
 
 import io.restassured.response.Response;
 import io.restassured.http.Headers;
-import org.json.JSONObject;
-import org.json.JSONArray;
 
+import org.json.simple.JSONObject;
 import utils.APIConstant;
 
 import java.util.HashMap;
@@ -17,32 +16,39 @@ public class UserHelper extends RequestHelper {
     public Response createUser(String name, String email, String gender, String status) {
         String url = prefixUrl + APIConstant.END_POINTS;
         JSONObject body = new JSONObject();
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(gender);
+        System.out.println(status);
+
         body.put("name", name);
         body.put("email", email);
-        body.put("gender",gender);
-        body.put("status",status);
+        body.put("gender", gender);
+        body.put("status", status);
         Map<String, String> map = new HashMap<>();
         map.put("Authorization", "Bearer " + APIConstant.PRIMARY_TOKEN);
+        Headers headers = createHeaders(map);
+        System.out.println(headers);
         Response response = sendRequest(
                 APIConstant.RequestType.POST,
                 url,
-                createHeaders(map),
+                headers,
                 body.toString());
         return response;
     }
 
-    public Response getAllUser() {
+    public Response getAllUser(String page) {
         String url = prefixUrl + APIConstant.END_POINTS;
         Map<String, String> map = new HashMap<>();
         map.put("Authorization", "Bearer " + APIConstant.PRIMARY_TOKEN);
         Response response = sendRequest(
                 APIConstant.RequestType.GET,
-                url,
+                url + "?page=" + page,
                 createHeaders(map), "");
         return response;
     }
 
-    public Response getUser(int userId) {
+    public Response getUser(String userId) {
         String url = prefixUrl + APIConstant.END_POINTS;
         Map<String, String> map = new HashMap<>();
         map.put("Authorization", "Bearer " + APIConstant.PRIMARY_TOKEN);
@@ -53,7 +59,7 @@ public class UserHelper extends RequestHelper {
         return  response;
     }
 
-    public Response UpdateUserDetails(String userId, String name, String email, String gender, String status) {
+    public Response updateUserDetails(String userId, String name, String email, String gender, String status) {
         String url = prefixUrl + APIConstant.END_POINTS;
         Map<String, String> map = new HashMap<>();
         map.put("Authorization", "Bearer " + APIConstant.PRIMARY_TOKEN);
@@ -79,7 +85,7 @@ public class UserHelper extends RequestHelper {
         return response;
     }
 
-    public Response DeleteUser(String userId) {
+    public Response deleteUser(String userId) {
         String url = prefixUrl + APIConstant.END_POINTS;
         Map<String, String> map = new HashMap<>();
         map.put("Authorization", "Bearer " + APIConstant.PRIMARY_TOKEN);
